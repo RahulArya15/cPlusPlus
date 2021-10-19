7816fac71d40022782a60cc28efcd81025c5b6ff
RahulArya15 <91099259+RahulArya15@users.noreply.github.com>
2021-10-19 11:26:12 +0530

    Update Practical_07_Task3_V4.cpp

--- Practical-07/Practical_07_Task3_V4.cpp
+++ Practical-07/Practical_07_Task3_V4.cpp
@@ -3,23 +3,24 @@
 using namespace std;
 int main()
 {
-	int a[MAX][MAX],count=1;
-	for(int i=0;i<MAX;i++)
+	int a[MAX][MAX]={{1,2,3},{4,5,6},{7,8,9}};
+	for(int i=0;i<MAX/2;i++)
 	{
-		for(int j=0;j<MAX;j++)
+		for(int j=0,k=2;j<MAX;j++,k--)
 		{
-			a[i][j]=count;
-			count++;
+			int temp;
+			temp=a[i][j];
+			a[i][j]=a[MAX-1-i][k];
+			a[MAX-1-i][k]=temp;	
 		}
 	}
 	for(int i=0;i<MAX;i++)
 	{
 		for(int j=0;j<MAX;j++)
 		{
-			cout<<10-a[i][j]<<" ";
+			cout<<a[i][j]<<" ";
 		}
 		cout<<"\n";
 	}
 	return 0;
 }
-
